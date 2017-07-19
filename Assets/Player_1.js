d5a7169c820fedf664336bf9ad10567a586d6ef7
David Rolfs and Hunter Parks <david+hunter@>
2017-07-19 16:53:19 -0700

    add more raw assets, functionality to knockback, background camera edit positioning

---
+++ Assets/Player_1.js
@@ -0,0 +1,77 @@
+﻿#pragma strict
+
+function Start () {
+    
+}
+
+var speed : float = 1.0;
+var health : float = 100.0;
+var Player3IsKnockedBack : boolean = false;
+var XVelocity : float = 0;
+var Direction : float = 0;
+
+function Update () {
+    CheckForDirection();
+
+    var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+    transform.position += move * speed * Time.deltaTime;
+
+    CheckForXVelocity();
+
+    if(Player3IsKnockedBack === true){
+        XVelocity--;
+        gameObject.Find("Player 3").GetComponent(Rigidbody2D).velocity = new Vector2(XVelocity, 0);
+    }
+
+    NoHealth();
+}
+
+function CheckForDirection(){
+    if(Input.GetAxis("Horizontal") > 0){
+        //0 is looking right
+        Direction = 0;
+    } else if (Input.GetAxis("Horizontal") < 0){
+        //1 is looking left
+        Direction = 1;
+    } else if (Input.GetAxis("Vertical") > 0){
+        //2 is looking up
+        Direction = 2;
+    } else {
+        //3 is looking down
+        Direction = 3;
+    }
+}
+
+function OnCollisionEnter2D(coll: Collision2D){
+    if(coll.gameObject.name == "Player 3" && Input.GetButton("Fire1")){
+        Player3KnockBack(coll);
+        coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(5, 0);
+        coll.gameObject.GetComponent(Player_2).health--;
+   }
+}
+
+function NoHealth(){
+    if(health <= 0){
+        Destroy(gameObject);
+    }
+}
+
+    // WARNIGN WARNIGN WARNIGN WARNING
+    // FIX HERE RIGHT NOW BELOW THE KNOCKBACK WORK ON IT
+
+function Player3KnockBack(coll: Collision2D){
+    Player3IsKnockedBack = true;
+    XVelocity = 10;
+    if(Direction == 0 || Direction == 3){
+        coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(Mathf.Abs(XVelocity), 0);
+    }
+    else{
+        coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(XVelocity, 0);
+    }
+}
+
+function CheckForXVelocity(){
+    if(XVelocity <= 0){
+        Player3IsKnockedBack = false;
+    }
+}
---
+++ Assets/Player_2.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+var speed : float = 1.0;
+var health : float = 100.0;
+
+function Update () {
+    var move = Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"), 0);
+    transform.position += move * speed * Time.deltaTime;
+
+    NoHealth();
+}
+
+function OnCollisionEnter2D(coll : Collision2D) {
+    if(coll.gameObject.name == "Player 1" && Input.GetButton("Fire2")){
+        //health -= 1;
+    }
+}
+
+function NoHealth(){
+    if(health <= 0){
+        Destroy(gameObject);
+    }
+}
