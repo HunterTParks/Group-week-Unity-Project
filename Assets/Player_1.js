042fa94477d6943045cde9cf25296446afd0acea
david rolfs and hunter parks <david+hunter@>
2017-07-20 12:14:20 -0700

    add movement boundries and fix bugs regarding knockback

--- Assets/Player_1.js
+++ Assets/Player_1.js
@@ -1,7 +1,7 @@
 ﻿#pragma strict
 
 function Start () {
-    
+ 
 }
 
 var speed : float = 1.0;
@@ -13,15 +13,21 @@ var Direction : float = 0;
 function Update () {
     CheckForDirection();
     CheckButtonPressForVelocity();
+    Boundry();
 
-    var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-    transform.position += move * speed * Time.deltaTime;
+    //var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+    //transform.position += move * speed * Time.deltaTime;
 
     CheckForXVelocity();
 
     if(Player3IsKnockedBack === true){
-        XVelocity--;
-        gameObject.Find("Player 3").GetComponent(Rigidbody2D).velocity = new Vector2(XVelocity, 0);
+        //XVelocity--;
+        if(XVelocity < 0){
+        	XVelocity++;
+        } else if (XVelocity > 0){
+        	XVelocity--;
+        }
+        //gameObject.Find("Player 3").GetComponent(Rigidbody2D).velocity = new Vector2(XVelocity, 0);
     }
 
     NoHealth();
@@ -29,19 +35,15 @@ function Update () {
 
 function CheckForDirection(){
     if(Input.GetAxis("Horizontal") > 0){
-    	Debug.Log("Right", gameObject);
         //0 is looking right
         Direction = 0;
     } else if (Input.GetAxis("Horizontal") < 0){
-    	Debug.Log("Left", gameObject);
         //1 is looking left
         Direction = 1;
     } else if (Input.GetAxis("Vertical") > 0){
-    	Debug.Log("Up", gameObject);
         //2 is looking up
         Direction = 2;
-    } else {
-    	Debug.Log("Down", gameObject);
+    } else if (Input.GetAxis("Vertical") < 0) {
         //3 is looking down
         Direction = 3;
     }
@@ -50,7 +52,6 @@ function CheckForDirection(){
 function OnCollisionEnter2D(coll: Collision2D){
     if(coll.gameObject.name == "Player 3" && Input.GetButton("Fire1")){
         Player3KnockBack(coll);
-        coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(5, 0);
         coll.gameObject.GetComponent(Player_2).health--;
    }
 }
@@ -65,14 +66,18 @@ function NoHealth(){
     // FIX HERE RIGHT NOW BELOW THE KNOCKBACK WORK ON IT
 
 function Player3KnockBack(coll: Collision2D){
+	var other = coll.gameObject;
     Player3IsKnockedBack = true;
-    XVelocity = 10;
-    if(Direction == 0 || Direction == 3){
-        coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(Mathf.Abs(XVelocity), 0);
+    if(Direction == 1 || Direction == 2){
+    	Debug.Log(Direction, gameObject);
+    	XVelocity = -10;
+    	coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(XVelocity, 0);
+    	//coll.gameObject.GetComponent(Rigidbody2D).AddForce(transform.right * -1);
+    } else if (Direction == 0 || Direction == 3) {
+    	XVelocity = 10;
+    	coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(XVelocity, 0);
     }
-    else{
-        coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(XVelocity, 0);
-    }
+    Debug.Log(Direction, gameObject);
 }
 
 function CheckForXVelocity(){
@@ -85,4 +90,29 @@ function CheckButtonPressForVelocity(){
 	if(!(Input.GetAxis("Horizontal") || Input.GetAxis("Vertical"))){
 		this.GetComponent(Rigidbody2D).velocity = new Vector2(0, 0);
 	} 
+}
+
+
+function Move(){
+    var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+    transform.position += move * speed * Time.deltaTime;
+
+}
+
+function Boundry(){
+	var boundry = gameObject.GetComponent(Transform).position;
+	var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+    if(boundry.y > -6.3 && Input.GetAxis("Vertical") > 0){
+    		Debug.Log("too far up");	
+    }	else if (boundry.y < -7.62 && Input.GetAxis("Vertical") < 0){
+    		Debug.Log("too far down");
+    }	else if (boundry.x < -12.9 && Input.GetAxis("Horizontal") < 0){
+    		Debug.Log("too far left");
+    }	else if (boundry.x > 4.34 && Input.GetAxis("Horizontal") > 0){
+    		Debug.Log("too far right");
+    } 	else {
+    	Move();
+    }
+
 }
--- Assets/Player_2.js
+++ Assets/Player_2.js
@@ -8,9 +8,10 @@ var speed : float = 1.0;
 var health : float = 100.0;
 
 function Update () {
-    var move = Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"), 0);
-    transform.position += move * speed * Time.deltaTime;
+	Boundry();
 
+
+	CheckButtonPressForVelocity();
     NoHealth();
 }
 
@@ -24,4 +25,37 @@ function NoHealth(){
     if(health <= 0){
         Destroy(gameObject);
     }
+}
+
+function CheckButtonPressForVelocity(){
+	if(!(Input.GetAxis("Horizontal") || Input.GetAxis("Vertical"))){
+		this.GetComponent(Rigidbody2D).velocity = new Vector2(0, 0);
+	} 
+}
+
+function Move(){
+    var move = Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"), 0);
+    transform.position += move * speed * Time.deltaTime;
+
+}
+
+function Boundry(){
+	var boundry = gameObject.GetComponent(Transform).position;
+	var move = Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"), 0);
+
+    if(boundry.y > -6 && Input.GetAxis("Vertical2") > 0){
+    		if(boundry.y > -5.9){
+    			boundry.y = -6;
+    		}
+    		Debug.Log("too far up");	
+    }	else if (boundry.y < -7.1 && Input.GetAxis("Vertical2") < 0 ){
+    		Debug.Log("too far down");
+    }	else if (boundry.x < -12.7 && Input.GetAxis("Horizontal2") < 0){
+    		Debug.Log("too far left");
+    }	else if (boundry.x > 4 && Input.GetAxis("Horizontal2") > 0){
+    		Debug.Log("too far right");
+    } 	else {
+    	Move();
+    }
+
 }
