ffa29f1cb724c23bdaa9f4e49d5f0d7301d669f9
Calvin Cushman <calvincushman@gmail.com>
2017-07-24 15:04:15 -0700

    add functionality to Animations and different movements

--- Assets/Scripts/Player_1.js
+++ Assets/Scripts/Player_1.js
@@ -1,9 +1,39 @@
 ﻿#pragma strict
 
 function Start () {
- 
+	
 }
 
+//head
+var headRight: Sprite;
+var headLeft: Sprite;
+var headBack: Sprite;
+var headFront: Sprite;
+//chest
+var chestRight: Sprite;
+var chestLeft: Sprite;
+var chestBack: Sprite;
+var chestFront: Sprite; 
+//arms
+var armRight: Sprite;
+var armLeft: Sprite;
+var armFrontL: Sprite;
+var armFrontR: Sprite;
+var armBackL: Sprite;
+var armBackR: Sprite;
+//legs
+var legRight: Sprite;
+var legLeft: Sprite;
+var legFrontL: Sprite;
+var legFrontR: Sprite;
+var legBackL: Sprite;
+var legBackR: Sprite;
+
+function Awake () {
+	//testSprite = gameObject.transform.Find("Chest").GetComponent(SpriteRenderer).sprite;
+}
+
+var currentSprite: Sprite;
 var speed : float = 1.0;
 var health : float = 100.0;
 var Player3IsKnockedBack : boolean = false;
@@ -36,18 +66,61 @@ function Update () {
 function CheckForDirection(){
     if(Input.GetAxis("Horizontal") > 0){
         //0 is looking right
+        Debug.Log("ETSTING RIGHT");
         Direction = 0;
+        TravelRight();
     } else if (Input.GetAxis("Horizontal") < 0){
         //1 is looking left
+        Debug.Log("ETSTING Left");
         Direction = 1;
+        TravelLeft();
     } else if (Input.GetAxis("Vertical") > 0){
         //2 is looking up
+        Debug.Log("ETSTING UP");
         Direction = 2;
+       	TravelUp();
     } else if (Input.GetAxis("Vertical") < 0) {
         //3 is looking down
+        Debug.Log("ETSTING Down");
         Direction = 3;
+        TravelDown();
     }
 }
+
+function TravelRight(){
+		gameObject.transform.Find("Head-front").GetComponent(SpriteRenderer).sprite = headRight;
+        gameObject.transform.Find("Chest").GetComponent(SpriteRenderer).sprite = chestRight;
+        gameObject.transform.Find("Right-leg").GetComponent(SpriteRenderer).sprite = legRight;
+        gameObject.transform.Find("Right-arm").GetComponent(SpriteRenderer).sprite = armRight;
+	}
+
+function TravelLeft(){
+		gameObject.transform.Find("Head-front").GetComponent(SpriteRenderer).sprite = headLeft;
+        gameObject.transform.Find("Chest").GetComponent(SpriteRenderer).sprite = chestLeft;
+        gameObject.transform.Find("Left-leg").GetComponent(SpriteRenderer).sprite = legLeft;
+        gameObject.transform.Find("Left-arm").GetComponent(SpriteRenderer).sprite = armLeft;
+        gameObject.transform.Find("Left-arm").GetComponent(Transform).position.x = gameObject.GetComponent(Transform).position.x;
+        gameObject.transform.Find("Left-arm").GetComponent(SpriteRenderer).flipX = true;
+	}
+
+function TravelUp(){
+		gameObject.transform.Find("Head-front").GetComponent(SpriteRenderer).sprite = headBack;
+        gameObject.transform.Find("Chest").GetComponent(SpriteRenderer).sprite = chestBack;
+        gameObject.transform.Find("Left-leg").GetComponent(SpriteRenderer).sprite = legBackL;
+        gameObject.transform.Find("Right-leg").GetComponent(SpriteRenderer).sprite = legBackR;
+        gameObject.transform.Find("Left-arm").GetComponent(SpriteRenderer).sprite = armBackR;
+        gameObject.transform.Find("Right-arm").GetComponent(SpriteRenderer).sprite = armBackL;
+	}
+
+function TravelDown(){
+		gameObject.transform.Find("Head-front").GetComponent(SpriteRenderer).sprite = headFront;
+        gameObject.transform.Find("Chest").GetComponent(SpriteRenderer).sprite = chestFront;
+        gameObject.transform.Find("Left-leg").GetComponent(SpriteRenderer).sprite = legFrontL;
+        gameObject.transform.Find("Right-leg").GetComponent(SpriteRenderer).sprite = legFrontR;
+        gameObject.transform.Find("Left-arm").GetComponent(SpriteRenderer).sprite = armFrontR;
+        gameObject.transform.Find("Right-arm").GetComponent(SpriteRenderer).sprite = armFrontL;
+	}
+
 
 function OnCollisionEnter2D(coll: Collision2D){
     if(coll.gameObject.name == "Player 3" && Input.GetButton("Fire1")){
@@ -91,7 +164,6 @@ function CheckButtonPressForVelocity(){
 		this.GetComponent(Rigidbody2D).velocity = new Vector2(0, 0);
 	} 
 }
-
 
 function Move(){
     var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
