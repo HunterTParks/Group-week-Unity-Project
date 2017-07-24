eee492218cd8fca6688d116b7231681b60968440
David Rolfs and Hunter Parks <david+hunter@>
2017-07-24 14:32:19 -0700

    add removing health funciton to player 2

---
+++ Assets/Scripts/Player 2 HealthBar.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+//var DeathBar: float = gameObject.GetComponent(Transform).position.x;
+var checkHealth: float = 100;
+
+//var Player: UnityEngine.GameObject = gameObject.Find("Player 1");
+
+var Player = gameObject.Find("Player 3");
+
+var LostHealth: float;
+
+function Start () {
+	
+}
+
+function FixedUpdate() {
+	
+	CheckForHealth();
+}
+
+
+function Update () {
+
+}
+
+
+function CheckForHealth() {
+
+	if(checkHealth != Player.GetComponent(Player_2).health){
+		Debug.Log(Player.GetComponent(Player_2).health);
+		Debug.Log(checkHealth);
+
+		var currentPosition = gameObject.GetComponent(Transform).position;
+
+		LostHealth = (checkHealth - Player.GetComponent(Player_2).health) * 0.0385;
+
+		var newPosition = currentPosition.x - LostHealth;
+		gameObject.GetComponent(Transform).position = Vector2(newPosition, currentPosition.y);
+
+		checkHealth = Player.GetComponent(Player_2).health;
+	}
+}
+
