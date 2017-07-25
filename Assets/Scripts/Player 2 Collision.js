99ebac0c93283b6b46e30005a2de5c9076270c5b
David Rolfs and Hunter Parks <david+hunter@>
2017-07-25 10:54:59 -0700

    add separation of script files for easier flow of code and organization; add bug fix to knockback

---
+++ Assets/Scripts/Player 2 Collision.js
@@ -0,0 +1,74 @@
+﻿#pragma strict
+
+var Player1IsKnockedBack : boolean = false;
+var XVelocity : float = 0;
+var Direction : float = 0;
+
+function Start () {
+	
+}
+
+function Update () {
+	CheckButtonPressForVelocity();
+	CheckForDirection();
+	CheckForXVelocity();
+}
+
+function FixedUpdate() {
+	 if(Player1IsKnockedBack === true){
+        if(XVelocity < 0){
+        	XVelocity++;
+        } else if (XVelocity > 0){
+        	XVelocity--;
+        }
+    }
+}
+
+function OnCollisionEnter2D(coll : Collision2D) {
+    if(coll.gameObject.name == "Player 1" && Input.GetButton("player2Push")){
+    	Player1KnockBack(coll);
+        coll.gameObject.GetComponent(Player_1).health-=10;
+    }
+}
+
+function Player1KnockBack(coll: Collision2D){
+    Player1IsKnockedBack = true;
+    Debug.Log(Direction);
+    if(Direction == 1 || Direction == 2){
+    	Debug.Log("TESTINGLOLOOLOLO");
+    	XVelocity = -20;
+    	coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(XVelocity, 0);
+    } else if (Direction == 0 || Direction == 3) {
+    	XVelocity = 20;
+    	coll.gameObject.GetComponent(Rigidbody2D).velocity = new Vector2(XVelocity, 0);
+    }
+}
+
+function CheckForXVelocity(){
+    if(XVelocity == 0){
+        Player1IsKnockedBack = false;
+    }
+}
+
+//TEST NOW FOR BuG ERRORS
+function CheckButtonPressForVelocity(){
+	if(!(Input.GetAxis("Horizontal") || Input.GetAxis("Vertical"))){
+		this.GetComponent(Rigidbody2D).velocity = new Vector2(0, 0);
+	} 
+}
+
+function CheckForDirection(){
+    if(Input.GetAxis("Horizontal2") > 0){
+        //0 is looking right
+        Direction = 0;
+    } else if (Input.GetAxis("Horizontal2") < 0){
+        //1 is looking left
+        Direction = 1;
+    } else if (Input.GetAxis("Vertical2") > 0){
+        //2 is looking up
+        Direction = 2;
+    } else if (Input.GetAxis("Vertical2") < 0) {
+        //3 is looking down
+        Direction = 3;
+    }
+}
---
+++ Assets/Scripts/Player_1_Movement.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var speed: float = 1.0;
+
+
+function Start () {
+	
+}
+
+function Update () {
+	Boundry();
+}
+
+function Boundry(){
+	var boundry = gameObject.GetComponent(Transform).position;
+	var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+    if(boundry.y > -6.3 && Input.GetAxis("Vertical") > 0){
+    }	else if (boundry.y < -7.62 && Input.GetAxis("Vertical") < 0){
+    }	else if (boundry.x < -12.9 && Input.GetAxis("Horizontal") < 0){
+    }	else if (boundry.x > 4.34 && Input.GetAxis("Horizontal") > 0){
+    } 	else {
+    	Move();
+    }
+}
+
+function Move(){
+	var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+    transform.position += move * speed * Time.deltaTime;
+}
