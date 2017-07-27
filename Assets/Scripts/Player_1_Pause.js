50f3d06d5302c0f8ba2712b13d9bcb7dcc561da5
Calvin Cushman <calvincushman@gmail.com>
2017-07-27 09:29:41 -0700

    adds broken pause features

---
+++ Assets/Scripts/Player_1_Pause.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var canvas: UnityEngine.GameObject;
+
+function Start () {
+	
+}
+
+function Update () {
+	Pause();
+}
+
+function Pause(){
+	if(Input.GetButtonDown("Start")){
+		if(Time.timeScale == 0){
+			Time.timeScale = 1;
+			canvas.SetActive(true);
+
+		} else {
+			Time.timeScale = 0;
+			canvas.SetActive(false);
+		}
+	}
+
+}
